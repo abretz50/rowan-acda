0bc70ba008c651705e06e5219340866e45919419
abretz50 <abretz50@gmail.com>
2025-08-14 17:04:00 -0400

    Add files via upload

---
+++ LoginDemo.java
@@ -0,0 +1,8 @@
+// Optional example Java file (not used by GitHub Pages)
+// Provided only because you asked for a 'java file' alongside images and css.
+// If you don't need Java, you can delete the /java folder.
+public class LoginDemo {
+    public static void main(String[] args){
+        System.out.println("This static site uses JS for the demo login. Java not required.");
+    }
+}
