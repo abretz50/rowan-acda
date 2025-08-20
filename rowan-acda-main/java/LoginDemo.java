118ddb328e75d6712176ef7fdfd60cc8b6937529
Adam Bretz <abretz50@gmail.com>
2025-08-20 18:43:51 -0400

    Desktop Commit

--- rowan-acda-main/java/LoginDemo.java
+++
@@ -1,7 +0,0 @@
-// Optional example Java file (not used by the website).
-// Included because you asked for a 'java file' alongside images and css.
-public class LoginDemo {
-    public static void main(String[] args){
-        System.out.println("This static site uses JavaScript for the demo login. Java is not required.");
-    }
-}
