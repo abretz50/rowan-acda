94c39ae6a3dc2472bb307219c70c796ac106afd5
abretz50 <abretz50@gmail.com>
2025-09-03 13:11:55 -0400

    Delete rowan-acda-main directory

--- rowan-acda-main/netlify/functions/_db.js
+++
@@ -1,7 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-
-export function sqlClient(){
-  const url = process.env.NEON_DATABASE_URL;
-  if(!url) throw new Error('Missing NEON_DATABASE_URL');
-  return neon(url);
-}
